462c4b0c4336b0bfd3e876d423234d90a798e6e7
pc <giorgisushkov@gmail.com>
2022-12-01 18:58:56 +0400

    comid one

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+
 })
-export class AppComponent {
-  title = 'ProjectOne';
+export class AppComponent implements OnInit{
+  public  exercise = [
+    'CV form',
+    'სახელი',
+    'გვარი',
+    'საკონტაკრო ინფორმაცია',
+    'რესურსები(github, linkdin)',
+    'სამუშაო გამოცდილება',
+    'განათლება'
+  ]
+
+  ngOnInit(){
+
+  }
 }
